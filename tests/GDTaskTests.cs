c283f12cd0ec865881ed08dbd98853c2e0b75aef
Timophey Khakhanov <daihaminkey@icloud.com>
2023-03-25 02:02:31 +0700

    Non-GDTask conversion

--- tests/GDTaskTests.cs
+++ tests/GDTaskTests.cs
@@ -3,7 +3,7 @@ using Fractural.Tasks;
 
 namespace Tests
 {
-    public class GDTaskTests : WAT.Test
+    public partial class GDTaskTests : WAT.Test
     {
         [Test]
         public void TODO_Make_Tests()
