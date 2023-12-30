75ccc878c5fc678f663a812f7b4e22e5c27d0420
DE-YU <delsin_yu@qq.com>
2023-12-31 03:48:01 +0800

    Update Player Loop TImer

--- addons/GDTask/PlayerLoopTimer.cs
+++ addons/GDTask/PlayerLoopTimer.cs
@@ -142,6 +142,7 @@ namespace Fractural.Tasks
 
     sealed class DeltaTimePlayerLoopTimer : PlayerLoopTimer
     {
+        bool isMainThread;
         ulong initialFrame;
         double elapsed;
         double interval;
@@ -156,7 +157,7 @@ namespace Fractural.Tasks
         {
             if (elapsed == 0.0)
             {
-                if (initialFrame == Engine.GetProcessFrames())
+                if (isMainThread && initialFrame == Engine.GetProcessFrames())
                 {
                     return true;
                 }
@@ -174,7 +175,8 @@ namespace Fractural.Tasks
         protected override void ResetCore(TimeSpan? interval)
         {
             this.elapsed = 0.0;
-            this.initialFrame = GDTaskPlayerLoopAutoload.IsMainThread ? Engine.GetProcessFrames() : 0ul;
+            this.isMainThread = GDTaskPlayerLoopAutoload.IsMainThread;
+            this.initialFrame = Engine.GetProcessFrames();
             if (interval != null)
             {
                 this.interval = (float)interval.Value.TotalSeconds;
